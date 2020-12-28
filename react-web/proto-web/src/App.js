147b1bf95bcb734e440eadd741f9a3d10894271c
donggoolosori <kdj6551@naver.com>
2020-12-28 23:26:34 +0900

    Create Header component

--- react-web/proto-web/src/App.js
+++ react-web/proto-web/src/App.js
@@ -1,30 +1,30 @@
-import './App.css';
+import "./App.css";
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <div id="LogoArea">
-          <div id="LogoCircle">
-            <div id="LogoPhoto"/>
-          </div>
-        </div>
-        <div id="Title">Son in My Hand</div>
-      </div>
+      <Header />
       <div className="Main">
         <div className="Intro">
-          This Webpage supports a YOLOv5 based Sonny Detector.<br/>
-          Upload a Tottenham video on the page and convert your video.<br/>
-          The converted video will show a picker that tracks Sonny along the field.<br/>
-          So, are you a big fan of Sonny?<br/>
-          Watch his every movement exclusively!<br/>
+          This Webpage supports a YOLOv5 based Sonny Detector.
+          <br />
+          Upload a Tottenham video on the page and convert your video.
+          <br />
+          The converted video will show a picker that tracks Sonny along the
+          field.
+          <br />
+          So, are you a big fan of Sonny?
+          <br />
+          Watch his every movement exclusively!
+          <br />
         </div>
-        <div className="VerticalLineArea"><div id="VerticalLine"></div></div>
+        <div className="VerticalLineArea">
+          <div id="VerticalLine"></div>
+        </div>
         <div className="UploadArea">
-          <div id="FileNameBox">
-            {/* <p>{fileName}</p> */}
-          </div>
+          <div id="FileNameBox">{/* <p>{fileName}</p> */}</div>
           <div className="ButtonArea">
             <div id="SearchFileButton">
               <button id="searchFileButton">Search File</button>
---
+++ react-web/proto-web/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="App-header">
+      <div id="LogoArea">
+        <div id="LogoCircle">
+          <div id="LogoPhoto" />
+        </div>
+      </div>
+      <div id="Title">Son in My Hand</div>
+    </div>
+  );
+};
+
+export default Header;
