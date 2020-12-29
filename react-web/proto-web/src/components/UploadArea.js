ee088a58ea487c57fab1803473915f03c325b4ab
donggoolosori <kdj6551@naver.com>
2020-12-29 13:36:32 +0900

    Change Button

--- react-web/proto-web/src/components/UploadArea.js
+++ react-web/proto-web/src/components/UploadArea.js
@@ -1,17 +1,49 @@
 import React from "react";
 import "./UploadArea.css";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import CachedIcon from "@material-ui/icons/Cached";
+
+const useStyles = makeStyles({
+  root: {
+    margin: "30px",
+    fontSize: "25px",
+  },
+  largeIcon: {
+    width: 40,
+    height: 40,
+  },
+  label: {
+    textTransform: "capitalize",
+  },
+});
 
 const UploadArea = () => {
+  const classes = useStyles();
+
   return (
     <div className="uploadArea">
       <div id="fileNameBox">{/* <p>{fileName}</p> */}</div>
       <div className="buttonArea">
-        <div id="searchFileButton__container">
-          <button id="searchFileButton">Search File</button>
-        </div>
-        <div id="convertButton__container">
-          <button id="convertButton">Convert video</button>
-        </div>
+        <Button
+          classes={{ root: classes.root }}
+          size="large"
+          variant="contained"
+          color="default"
+          startIcon={<CloudUploadIcon className={classes.largeIcon} />}
+        >
+          Upload
+        </Button>
+        <Button
+          classes={{ root: classes.root }}
+          size="large"
+          variant="contained"
+          color="primary"
+          startIcon={<CachedIcon className={classes.largeIcon} />}
+        >
+          Convert
+        </Button>
       </div>
     </div>
   );
